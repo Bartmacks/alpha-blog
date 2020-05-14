c59e329920e822ff3e0d943a1e5f9c3ce29f4b6e
Owner <daniel.bender@arcor.de>
2020-05-14 22:21:45 +0200

    Add one-to-many association between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     @article.save
     if @article.save
       flash[:notice] = "Article was created successfully."
