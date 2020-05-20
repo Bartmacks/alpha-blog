603d1c0b0b8248ff4bfce984424bf7b9ee583800
Owner <daniel.bender@arcor.de>
2020-05-20 16:57:21 +0200

    Add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    if @user.save
+      flash[:notice] = "User was saved successfully"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest)
+  end
 end
