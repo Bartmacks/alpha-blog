7066850630e29169d03c3ab3aa0520cc1816b4b3
Owner <daniel.bender@arcor.de>
2020-05-25 23:00:25 +0200

    Add pagination to views

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem "better_errors"
   gem "binding_of_caller"
+  gem 'will_paginate', '~> 3.3.0'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,11 +35,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   def user_params
     params.require(:user).permit(:username, :email, :password_digest)
